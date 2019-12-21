417d4d1e7bcc8b4c4c7273fe69232ef76ee9aa6d
Marat-R <you@example.com>
2019-12-21 11:58:13 +0600

    first commit

---
+++ Task3.py
@@ -0,0 +1,11 @@
+number = input("Enter number: ")
+
+next_number = int(number) + 1
+prev_number = int(number) - 1
+
+# print("The next number for the number " + str(number) + " is " + str(next_number) + ".")
+# print("The previous number for the number " + str(number) + " is " + str(prev_number) + ".")
+
+
+print(f"The next number for the number {number} is {next_number}.")
+print(f"The previous number for the number {number} is {prev_number}.")
